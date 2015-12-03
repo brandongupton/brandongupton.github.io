e5987f5d60d2886f44ccddd523bf2dc3b80fb5d3
Brandon Gupton <brandon.gupton@cookmedical.com>
2015-12-03 08:00:53 -0500

    Removing twitter button, works from file:// but not when pushed to site

--- js/quote-generator.js
+++ js/quote-generator.js
@@ -8,24 +8,11 @@ $(document).ready(function() {
       url: "https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous",
       success: function(data) {
         var response = JSON.parse(data);
-        var tweetString = '"' + response.quote + '" ~ ' + response.author;
         console.log('response: ' + data);
         console.log('quote: ' + response.quote);
         console.log('author: ' + response.author);
         $('#quote').text(response.quote);
         $('#author').text(response.author);
-
-        // Remove existing anchor and iframe
-        $('#tweetBtn a').remove();
-        $('#tweetBtn iframe').remove();
-        // Generate new markup
-        var tweetBtn = $('<a></a>')
-            .addClass('twitter-share-button')
-            .attr('href', 'https://twitter.com/share')
-            .attr('data-size', 'large')
-            .attr('data-text', tweetString)
-        $('#tweetBtn').append(tweetBtn);
-        twttr.widgets.load();
       }
     });
   }
